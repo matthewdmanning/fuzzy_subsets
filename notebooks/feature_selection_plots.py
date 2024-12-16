e68dc41e4ed8e9535d9fee6019e5e38b952e780f
MMANNING <manning.matthew@epa.gov>
2024-12-16 08:43:44 -0500

    Reshapes inputs using melt_cv_scores. Extracts boxplot function.
    Signed-off-by: MMANNING <manning.matthew@epa.gov>

--- notebooks/feature_selection_plots.py
+++ notebooks/feature_selection_plots.py
@@ -9,6 +9,8 @@ import seaborn as sns
 from sklearn import clone
 from sklearn.model_selection import StratifiedKFold
 
+import dataset_visualize
+
 
 def separate_floats_and_strs(mixed_list):
     float_list, str_list = list(), list()
@@ -20,7 +22,7 @@ def separate_floats_and_strs(mixed_list):
     return float_list, str_list
 
 
-def get_score_features(feature_dir, score_path):
+def get_selection_scores(feature_dir, score_path, repeat_size=None):
     if os.path.isdir(feature_dir) and os.path.isfile(score_path):
         with open(score_path, encoding="utf-8") as f:
             best_list = f.readlines()
@@ -31,6 +33,10 @@ def get_score_features(feature_dir, score_path):
     for line in best_list:
         score_feats = line.split("\t")
         line_scores, line_feats = separate_floats_and_strs(score_feats)
+        if repeat_size is not None:
+            line_scores = [
+                np.mean(t) for t in itertools.batched(line_scores, n=repeat_size)
+            ]
         scores.append(tuple(line_scores))
         feature_lists.append([s.strip() for s in line_feats])
         if len(feature_lists[-1]) == 0:
@@ -40,11 +46,22 @@ def get_score_features(feature_dir, score_path):
         index=list(range(len(scores))),
         columns=["Scores", "n_features"],
     )
-    return score_feat_df, feature_lists
+    raw_score_subset_df = pd.DataFrame(
+        data=(list(zip(scores, feature_lists))),
+        index=list(range(len(scores))),
+        columns=["Scores", "Subsets"],
+    )
+    return score_feat_df, feature_lists, raw_score_subset_df
 
 
-def heirarchical_predictions(
-    feature_df, labels, model, features_lists, cv=(5, 5), binary=False, save_dir=None
+def get_subset_predictions(
+    feature_df,
+    labels,
+    model,
+    features_lists,
+    cv=(5, 5),
+    output="predict",
+    save_dir=None,
 ):
     # feat_cv_preds_dict: stores CV predictions for all sets
     # cv_members = pd.DataFrame(index=labels.index, columns=["Split", "Repeat"])
@@ -91,8 +108,12 @@ def heirarchical_predictions(
                 new_model = clone(estimator=model)
                 for set_ix, feature_list in enumerate(features_lists):
                     retrained_model = new_model.fit(X=train_df[feature_list], y=train_y)
-                    if binary:
+                    if output == "predict":
                         new_predicts = retrained_model.predict(X=test_df[feature_list])
+                    elif "log" in output:
+                        new_predicts = retrained_model.predict_log_proba(
+                            X=test_df[feature_list]
+                        )
                     else:
                         new_predicts = retrained_model.predict_proba(
                             X=test_df[feature_list]
@@ -210,74 +231,200 @@ def average_subset_repeats(score_df_dict):
     return mean_scores_df
 
 
-def grab_selection_results(feature_dir, best_only=True):
+def grab_selection_results(feature_dir, best_only=True, repeat_size=None):
     score_path = "{}feature_score_path.csv".format(feature_dir)
     if not os.path.isdir(feature_dir) or not os.path.isfile(score_path):
-        return None, None, None, None
-    score_feat_df, feature_subsets = get_score_features(feature_dir, score_path)
+        return None, None, None, None, None
+    score_feat_df, feature_subsets, raw_score_subset_df = get_selection_scores(
+        feature_dir, score_path, repeat_size=repeat_size
+    )
     if score_feat_df is None:
-        return None, None, None, None
+        return None, None, None, None, None
     print("Scoring results retrieved.")
     score_feat_df["Mean"] = score_feat_df["Scores"].apply(np.mean)
     score_feat_df["Std"] = score_feat_df["Scores"].apply(np.std)
     score_feat_df["AdjMean"] = score_feat_df["Mean"] - score_feat_df["Std"]
-    score_feat_df.sort_values(by="AdjMean", ascending=False).drop_duplicates(
-        subset="n_features", inplace=True
-    )
+    if best_only:
+        score_feat_df.sort_values(by="AdjMean", ascending=False).drop_duplicates(
+            subset="n_features", inplace=True
+        )
     print("Score Summary")
     pprint.pp(
         score_feat_df[["Mean", "Std", "AdjMean"]]
         .sort_values(by="AdjMean", ascending=False)
         .head(),
         compact=True,
-        width=100,
+        width=140,
     )
     best_adj = score_feat_df["AdjMean"].max()
     score_feat_df.drop(columns=["Mean", "Std", "AdjMean"], inplace=True)
-    separate_scores_df = pd.DataFrame(
+    only_scores_df = pd.DataFrame(
         score_feat_df["Scores"].to_list(), index=score_feat_df.index
     )
-    separate_scores_df.columns = [
-        "Repeat_{}".format(i) for i in np.arange(separate_scores_df.shape[1])
+    only_scores_df.columns = [
+        "Fold_{}".format(i) for i in np.arange(only_scores_df.shape[1])
     ]
     separate_scores_df = pd.concat(
-        [separate_scores_df, score_feat_df["n_features"]], copy=True, axis=1
+        [only_scores_df, score_feat_df["n_features"]], copy=True, axis=1
     )
     # print(separate_scores_df, score_feat_df, feature_subsets, best_adj)
     # score_feat_df["Grove"] = grove_names[i].split("\t")[0]
-    return separate_scores_df, score_feat_df, feature_subsets, best_adj
+    return (
+        separate_scores_df,
+        score_feat_df,
+        feature_subsets,
+        best_adj,
+        raw_score_subset_df,
+    )
 
 
 def main():
-    model_nick, full_model_name = "logit", "Logistic Regression"
-    model_dir = "{}{}_grove_features_3/".format(os.environ.get("MODEL_DIR"), model_nick)
-    grove_names = pd.read_csv(
-        "{}feature_names.csv".format(model_dir), index_col=0, sep="\t"
-    ).squeeze()
-    grove_names.name = "Grove"
-    scores_df_dict, grove_df_dict, features_dict, best_adj_dict = (
+    groves = False
+    model_nick, full_model_name = "rfc", "Random Forest"
+    feature_dirs = list()
+    model_dir = "{}{}_all_samples_1/".format(os.environ.get("MODEL_DIR"), model_nick)
+    if groves:
+        grove_names = pd.read_csv(
+            "{}feature_names.csv".format(model_dir), index_col=0, sep="\t"
+        ).squeeze()
+        grove_names.name = "Grove"
+        grove_nick = [
+            "{}   {} {}".format(x[:50], y[0], y[1]) for x, y in grove_names.iterrows()
+        ]
+        for g_ix in np.arange(50):
+            fdir = "{}{}/".format(model_dir, g_ix)
+            if os.path.isdir(fdir):
+                feature_dirs.append(fdir)
+    else:
+        feature_dirs = ["{}".format(model_dir)]
+        grove_nick = ["All Samples"]
+
+    scores_df_dict, grove_df_dict, features_dict, best_adj_dict, score_subset_dict = (
+        dict(),
         dict(),
         dict(),
         dict(),
         dict(),
     )
     # grove_nick = grove_names.index.map(mapper=lambda x: x[: min(len(x), 40)]).tolist()
-    for i in list(range(0, 50)):
-        feature_dir = "{}{}/".format(model_dir, i)
-        s, g, f, b = grab_selection_results(feature_dir)
-        if any([a is None for a in [s, g, f, b]]):
+    for i, feature_dir in enumerate(feature_dirs):
+        member_labels = pd.read_csv(
+            "{}member_labels.csv".format(feature_dir), index_col=0
+        )
+        preprocessed_df = pd.read_pickle(
+            "{}preprocessed_feature_df.pkl".format(feature_dir)
+        )
+        s, g, f, b, r = grab_selection_results(feature_dir, repeat_size=5)
+        if any([a is None for a in [s, g, f, b, r]]):
             break
-        grove_df_dict[i], features_dict[i], best_adj_dict[i] = g, f, b
-    print(s)
-    grove_df_dict = dict(
-        sorted(grove_df_dict.items(), key=lambda x: best_adj_dict[x[0]], reverse=True)
-    )
-    # sns.set_context("talk")
-    grove_nick = [
-        "{}   {} {}".format(x[:50], y[0], y[1]) for x, y in grove_names.iterrows()
-    ]
-    grove_list = list(grove_df_dict.values())
+        grove_df_dict[i], features_dict[i], best_adj_dict[i], score_subset_dict[i] = (
+            g,
+            f,
+            b,
+            r,
+        )
+        grove_df_dict = dict(
+            sorted(
+                grove_df_dict.items(), key=lambda x: best_adj_dict[x[0]], reverse=True
+            )
+        )
+        if not groves:
+            for score_subset_df in score_subset_dict.values():
+                print(preprocessed_df)
+                print(score_subset_df)
+                score_subset_df["Mean"] = score_subset_df["Scores"].apply(np.mean)
+                best_subset = score_subset_df.sort_values(by="Mean", ascending=False)[
+                    "Subsets"
+                ].iloc[0]
+                print(preprocessed_df[best_subset][preprocessed_df[best_subset].isna()])
+                dataset_visualize.dueling_violins(
+                    preprocessed_df[best_subset], member_labels
+                )
+        # sns.set_context("talk")
+        grove_list = list(grove_df_dict.values())
+    # Boxplots
+    # Scatter n_features
+    # long_list = melt_cv_scores(grove_list, grove_nick)
+    # long_list = melt_cv_scores(grove_list, grove_nick)
+    # print(long_list)
+    # subsets_facet_boxplots(full_model_name, long_list, model_dir)
+    # faceted_scatter(full_model_name, long_list, model_dir)
+    # plt.show()
+
+
+def faceted_scatter(full_model_name, long_list, model_dir):
+    for i, (grove_dfs) in enumerate(itertools.batched(long_list, n=4)):
+        long_df = pd.concat(grove_dfs)
+        max_n_feats = long_df["n_features"].max()
+        fbounds = np.ceil(max_n_feats / 5) * 5
+        score_grid = sns.FacetGrid(
+            long_df,
+            col="Grove",
+            col_wrap=2,
+            height=4,
+            xlim=(0, fbounds),
+            ylim=(0.5, 1.0),
+        )
+        print(long_df)
+        # grove_df = pd.concat(grove_dfs).pivot(columns=["n_features"]) # .explode(column="Scores", ignore_index=True)
+        score_grid.map(
+            sns.scatterplot,
+            data=long_df,
+            x="n_features",
+            y="score",
+            hue="fold",
+            palette="rocket",
+            sizes=1,
+            alpha=0.25,
+            hue_order=long_df["fold"].unique().tolist(),
+        )
+        score_grid.set_axis_labels("Num Features", "CV Scores")
+        score_grid.set(
+            xticks=np.arange(0, np.arange(0, fbounds + 1, 5)),
+            yticks=[0.5, 0.6, 0.7, 0.8, 0.9],
+        )
+        score_grid.figure.subplots_adjust(wspace=0.02, hspace=0.2)
+        score_grid.savefig("{}n_feature_score_scatter_{}.png".format(model_dir, i))
+        score_grid.fig.subplots_adjust(top=0.9)
+        score_grid.fig.suptitle(full_model_name)
+
+
+def subsets_facet_boxplots(full_model_name, long_list, model_dir):
     sns.set_style("whitegrid")
+    [df.drop(columns=["fold"], inplace=True) for df in long_list]
+    for i, (grove_dfs) in enumerate(itertools.batched(long_list, n=4)):
+        long_df = pd.concat(grove_dfs)
+        max_n_feats = long_df["n_features"].max()
+        fbounds = np.ceil(max_n_feats / 5) * 5
+        score_grid = sns.FacetGrid(
+            long_df,
+            col="Grove",
+            col_wrap=2,
+            height=4,
+            xlim=(0, fbounds + 1),
+            ylim=(0.49, 1.0),
+        )
+        print(long_df)
+        # grove_df = pd.concat(grove_dfs).pivot(columns=["n_features"]) # .explode(column="Scores", ignore_index=True)
+        score_grid.map(
+            sns.boxplot,
+            "n_features",
+            "score",
+            order=list(np.arange(2, fbounds + 1)),
+            native_scale=True,
+        )
+        # score_grid.map(sns.boxplot, grove_df, "n_features", scores.columns, native_scale=True, ax=axs[i])
+        score_grid.set_axis_labels("Num Features", "CV Scores")
+        score_grid.set(
+            xticks=np.arange(0, fbounds + 1, 5), yticks=[0.5, 0.6, 0.7, 0.8, 0.9]
+        )
+        score_grid.figure.subplots_adjust(wspace=0.02, hspace=0.2)
+        score_grid.savefig("{}feature_selection_best_{}.png".format(model_dir, i))
+        score_grid.fig.subplots_adjust(top=0.85)
+        score_grid.fig.suptitle(full_model_name)
+
+
+def melt_cv_scores(grove_list, grove_nick):
     long_list = list()
     for g_i, (grove_df) in enumerate(grove_list):
         scores = pd.DataFrame(grove_df["Scores"].to_list())
@@ -285,36 +432,12 @@ def main():
         scores.columns = scores_names
         grove_df = pd.concat([scores, grove_df["n_features"].astype(int)], axis=1)
         grove_df["Grove"] = grove_nick[g_i]
-        long_list.append(grove_df.melt(
-            id_vars=["n_features", "Grove"], var_name="fold", value_name="score"))
-    for i, (grove_dfs) in enumerate(itertools.batched(long_list, n=4)):
-        long_df = pd.concat(grove_dfs)
-        score_grid = sns.FacetGrid(
-            long_df,
-            col="Grove",
-            col_wrap=2,
-            height=4,
-            xlim=(0, 31),
-            ylim=(0.5, 1.0),
+        long_list.append(
+            grove_df.melt(
+                id_vars=["n_features", "Grove"], var_name="fold", value_name="score"
+            )
         )
-        print(long_df)
-        # grove_df = pd.concat(grove_dfs).pivot(columns=["n_features"]) # .explode(column="Scores", ignore_index=True)
-        score_grid.map(
-            sns.boxplot,
-            data=long_df,
-            x="n_features",
-            y="score",
-            order=list(np.arange(2,31)),
-            native_scale=True,
-        )
-        # score_grid.map(sns.boxplot, grove_df, "n_features", scores.columns, native_scale=True, ax=axs[i])
-        score_grid.set_axis_labels("Num Features", "CV Scores")
-        score_grid.set(xticks=[0, 10, 20, 30], yticks=[0.5, 0.6, 0.7, 0.8, 0.9])
-        score_grid.figure.subplots_adjust(wspace=0.02, hspace=0.2)
-        score_grid.savefig("{}feature_selection_best_{}.png".format(model_dir, i))
-        score_grid.fig.subplots_adjust(top=0.9)
-        score_grid.fig.suptitle(full_model_name)
-    # plt.show()
+    return long_list
 
 
 if __name__ == "__main__":
