3b95662e6d9f2ed2b16dfb226d49433500bf4dd7
MMANNING <manning.matthew@epa.gov>
2025-02-18 14:13:49 -0500

    Adds function to calculate Kendall's Tau-c.
    Signed-off-by: MMANNING <manning.matthew@epa.gov>

--- qsar_modeling/utils/math_tools.py
+++ qsar_modeling/utils/math_tools.py
@@ -1,5 +1,10 @@
 import numpy as np
 import pandas as pd
+from scipy.stats import kendalltau
+
+
+def calculate_kendalls_c(first_series, second_series):
+    return kendalltau(first_series, second_series, nan_policy="raise", variant="c").statistic
 
 
 def softmax(X, axis=0):
