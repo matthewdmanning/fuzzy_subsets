3fcc7abe77dad2ed87eb785520cc46f4b9b1651f
MMANNING <manning.matthew@epa.gov>
2025-04-07 11:05:37 -0400

    Separates sample-weighted fitting commands.
    Signed-off-by: MMANNING <manning.matthew@epa.gov>

--- qsar_modeling/utils/parallel_subsets.py
+++ qsar_modeling/utils/parallel_subsets.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from inspect import signature
 
 import joblib
 from joblib import delayed, Parallel
@@ -11,16 +12,29 @@ def train_model_subsets(
 ):
     logging.debug(predictor_list)
     with joblib.parallel_config(temp_folder=os.environ.get("JOBLIB_TMP")):
-        fit_models = Parallel(
-            n_jobs=joblib.parallel.cpu_count() - 3,
-            prefer="processes",
-            temp_folder=mem_dir,
-        )(
-            delayed(clone_model(model).fit)(
-                X=feature_df[predictors[0]],
-                y=feature_df[predictors[1]],
-                sample_weight=sample_weights,
+        if "sample_weight" in signature(model.fit).parameters.keys():
+            fit_models = Parallel(
+                # n_jobs=joblib.parallel.cpu_count() - 3,
+                prefer="processes",
+                temp_folder=mem_dir,
+            )(
+                delayed(clone_model(model).fit)(
+                    X=feature_df[predictors[0]],
+                    y=feature_df[predictors[1]],
+                    sample_weight=sample_weights,
+                )
+                for predictors in predictor_list
             )
-            for predictors in predictor_list
-        )
+        else:
+            fit_models = Parallel(
+                # n_jobs=joblib.parallel.cpu_count() - 3,
+                prefer="processes",
+                temp_folder=mem_dir,
+            )(
+                delayed(clone_model(model).fit)(
+                    X=feature_df[predictors[0]],
+                    y=feature_df[predictors[1]],
+                )
+                for predictors in predictor_list
+            )
     return fit_models
